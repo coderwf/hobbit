56843e8dce2830741ae843d620838371d7ecf21b
wangdongwei <wangdongwei@bytedance.com>
2019-03-31 17:40:56 +0800

    init repos

---
+++ post/const.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+
+class PostPermission:
+    identity_key = "post_permission"
+    can_post_post_permission = "can_post_post_permission"
+    can_delete_self_post_permission = "can_delete_self_post_permission"
+    can_delete_post_permission = "can_delete_post_permission"
+    can_view_post_permission = "can_view_post_permission"
+    can_reply_post_permission = "can_reply_post_permission"
+    can_modify_self_post_permission = "can_modify_self_post_permission"
+    can_modify_post_permission = "can_modify_post_permission"
